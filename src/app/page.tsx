bdb7c8f549f127d31f349824c50aa1699c3f9598
DanielM-H <e-danhil1@iskule.no>
2024-02-01 14:20:36 +0100

    Just put in a picture, havent fixed the height or width yet. Planning to make a portfolio.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
   return (
     <main>
-      
+    <Image 
+      src="/Ski.jpeg" 
+      alt={"Skibilde"} 
+      height={1000} 
+      width={1000} 
+    />
+    <link rel="stylesheet" href="https://styreportalen.vercel.app/"></link>
+    
     </main>
-
   );
 }
